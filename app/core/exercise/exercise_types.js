eb09202a5ace9b30934ff4a4d938dc8381224c99
Steven Oxley <steven.aj.oxley@gmail.com>
2017-12-04 22:38:34 -0800

    fix: add missing exercises.

--- app/core/exercise/exercise_types.js
+++ app/core/exercise/exercise_types.js
@@ -145,5 +145,303 @@ export const ExercisesByGroup = {
       id: 'back-18',
       name: 'T-Bar Row'
     }
+  ],
+  biceps: [
+    {
+      id: 'biceps-1',
+      name: 'Barbell Curl'
+    },
+    {
+      id: 'biceps-2',
+      name: 'Cable Curl'
+    },
+    {
+      id: 'biceps-3',
+      name: 'Dumbbell Concentration Curl'
+    },
+    {
+      id: 'biceps-4',
+      name: 'Dumbbell Curl'
+    },
+    {
+      id: 'biceps-5',
+      name: 'Dumbbell Hammer Curl'
+    },
+    {
+      id: 'biceps-6',
+      name: 'Dumbbell Preacher Curl'
+    },
+    {
+      id: 'biceps-7',
+      name: 'EZ-Bar Curl'
+    },
+    {
+      id: 'biceps-8',
+      name: 'EZ-Bar Preacher Curl'
+    },
+    {
+      id: 'biceps-9',
+      name: 'Seated Incline Dumbbell Curl'
+    },
+    {
+      id: 'biceps-10',
+      name: 'Seated Machine Curl'
+    }
+  ]
+  chest: [
+    {
+      id: 'chest-1',
+      name: 'Cable Crossover'
+    },
+    {
+      id: 'chest-2',
+      name: 'Decline Barbell Bench Press'
+    },
+    {
+      id: 'chest-3',
+      name: 'Decline Hammer Strength Chest Press'
+    },
+    {
+      id: 'chest-4',
+      name: 'Dumbbell Bench Press'
+    },
+    {
+      id: 'chest-5',
+      name: 'Flat Barbell Bench Press'
+    },
+    {
+      id: 'chest-6',
+      name: 'Flat Dumbbell Bench Press'
+    },
+    {
+      id: 'chest-7',
+      name: 'Flat Dumbbell Fly'
+    },
+    {
+      id: 'chest-8',
+      name: 'Incline Barbell Bench Press'
+    },
+    {
+      id: 'chest-9',
+      name: 'Incline Dumbbell Bench Press'
+    },
+    {
+      id: 'chest-10',
+      name: 'Incline Dumbbell Fly'
+    },
+    {
+      id: 'chest-11',
+      name: 'Incline Hammer Strength Chest Press'
+    },
+    {
+      id: 'chest-12,
+      name: 'Push Up'
+    },
+    {
+      id: 'chest-13',
+      name: 'Seated Machine Fly'
+    }
+  ],
+  legs: [
+    {
+      id: 'legs-1',
+      name: 'Barbell Calf Raise'
+    },
+    {
+      id: 'legs-2',
+      name: 'Barbell Front Squat'
+    },
+    {
+      id: 'legs-3',
+      name: 'Barbell Glute Bridge'
+    },
+    {
+      id: 'legs-4',
+      name: 'Barbell Squat'
+    },
+    {
+      id: 'legs-5',
+      name: 'Donkey Calf Raise'
+    },
+    {
+      id: 'legs-6',
+      name: 'Dumbbell Calf Raise'
+    },
+    {
+      id: 'legs-7',
+      name: 'Glute-Ham Raise'
+    },
+    {
+      id: 'legs-8',
+      name: 'Hex Bar Deadlift'
+    },
+    {
+      id: 'legs-9',
+      name: 'Leg Extension Machine'
+    },
+    {
+      id: 'legs-10',
+      name: 'Leg Press'
+    },
+    {
+      id: 'legs-11',
+      name: 'Lunge'
+    },
+    {
+      id: 'legs-12',
+      name: 'Lying Leg Curl Machine'
+    },
+    {
+      id: 'legs-13',
+      name: 'Romanian Deadlift'
+    },
+    {
+      id: 'legs-14',
+      name: 'Seated Calf Raise Machine'
+    },
+    {
+      id: 'legs-15,
+      name: 'Seated Leg Curl Machine'
+    },
+    {
+      id: 'legs-16',
+      name: 'Standing Calf Raise Machine'
+    },
+    {
+      id: 'legs-17',
+      name: 'Stiff-Legged Deadlift'
+    },
+    {
+      id: 'legs-18',
+      name: 'Sumo Deadlift'
+    }
+  ],
+  shoulders: [
+    {
+      id: 'shoulders-1',
+      name: 'Arnold Dumbbell Press'
+    },
+    {
+      id: 'shoulders-2',
+      name: 'Behind The Neck Barbell Press'
+    },
+    {
+      id: 'shoulders-3',
+      name: 'Cable Face Pull'
+    },
+    {
+      id: 'shoulders-4',
+      name: 'Front Dumbbell Raise'
+    },
+    {
+      id: 'shoulders-5',
+      name: 'Hammer Strength Shoulder Press'
+    },
+    {
+      id: 'shoulders-6',
+      name: 'Lateral Dumbbell Raise'
+    },
+    {
+      id: 'shoulders-7',
+      name: 'Lateral Machine Raise'
+    },
+    {
+      id: 'shoulders-8',
+      name: 'Log Press'
+    },
+    {
+      id: 'shoulders-9',
+      name: 'One-Arm Standing Dumbbell Press'
+    },
+    {
+      id: 'shoulders-10',
+      name: 'Overhead Press'
+    },
+    {
+      id: 'shoulders-11',
+      name: 'Push Press'
+    },
+    {
+      id: 'shoulders-12',
+      name: 'Rear Delt Dumbbell Raise'
+    },
+    {
+      id: 'shoulders-13',
+      name: 'Rear Delt Machine Fly'
+    },
+    {
+      id: 'shoulders-14',
+      name: 'Seated Dumbbell Lateral Raise'
+    },
+    {
+      id: 'shoulders-15,
+      name: 'Seated Dumbbell Press'
+    },
+    {
+      id: 'shoulders-16',
+      name: 'Shrugs'
+    },
+    {
+      id: 'shoulders-17',
+      name: 'Smith Machine Overhead Press'
+    }
+  ],
+  triceps: [
+    {
+      id: 'triceps-1',
+      name: 'Cable Overhead Triceps Extension'
+    },
+    {
+      id: 'triceps-2',
+      name: 'Chair Dips'
+    },
+    {
+      id: 'triceps-3',
+      name: 'Close Grip Barbell Bench Press'
+    },
+    {
+      id: 'triceps-4',
+      name: 'Dumbbell Overhead Triceps Extension'
+    },
+    {
+      id: 'triceps-5',
+      name: 'EZ-Bar Skullcrusher'
+    },
+    {
+      id: 'triceps-6',
+      name: 'Lying Triceps Extension'
+    },
+    {
+      id: 'triceps-7',
+      name: 'Machine Arm Extension'
+    },
+    {
+      id: 'triceps-8',
+      name: 'One Arm Overhead Dumbbell Extension'
+    },
+    {
+      id: 'triceps-9',
+      name: 'Parallel Bar Triceps Dip'
+    },
+    {
+      id: 'triceps-10',
+      name: 'Ring Dip'
+    },
+    {
+      id: 'triceps-11',
+      name: 'Rope Push Down'
+    },
+    {
+      id: 'triceps-12',
+      name: 'Smith Machine Close Grip Bench Press'
+    },
+    {
+      id: 'triceps-13',
+      name: 'Triceps Kickback'
+    },
+    {
+      id: 'triceps-14',
+      name: 'V-Bar Push Down'
+    }
   ]
 };
